8193c6b4a2ade34c0b70d8e19f0c6da458e20570
veronica <veronica.pecchioli@outlook.it>
2018-11-14 13:24:40 +0100

    Initial commit

---
+++ cmake-build-debug/CMakeFiles/MatrixTemplate.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/MatrixTemplate.dir/main.cpp.o"
+  "MatrixTemplate.pdb"
+  "MatrixTemplate"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/MatrixTemplate.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
