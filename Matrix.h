5cd9b9a2b81db1551f1703eea9cdf08f61a4f175
veronica <veronica.pecchioli@outlook.it>
2019-01-18 17:53:52 +0100

    Add Matrix.h with implementations of some functions

--- Matrix.h
+++ Matrix.h
@@ -5,10 +5,13 @@
 #ifndef MATRIXTEMPLATE_MATRIX_H
 #define MATRIXTEMPLATE_MATRIX_H
 
+#include <iostream>
 #include <utility>
+#include <vector>
 #include <stdexcept>
 #include <cstdlib>
 using namespace std;
+
 
 template <typename T>
 class Matrix{
@@ -16,41 +19,44 @@ class Matrix{
 private:
     int rows;
     int col;
-    T * M;
+    T *M;
 
 public:
 
-  Matrix(int row, int col) throw(out_of_range); // costruttore con numero righe e colonne per matrici rettangolari
-  explicit Matrix(int size) throw(out_of_range);  // costruttore con size per matrici quadrate
+  Matrix(int row,int col) throw(out_of_range); // costruttore per matrici rettangolari
+  explicit Matrix(int size) throw(out_of_range);  // costruttore per quadrate
+  Matrix (int rows, int col, const vector<T>& v)throw(out_of_range); // costruttore con vettore di valori da inserire nella matrice
+  Matrix (int size, const vector <T>&v) throw(out_of_range);
+  Matrix(const Matrix<T>& Matrix); // costruttore di copia
   virtual ~ Matrix();
 
-
-  Matrix <T> trasponse(); // trasposta
-  Matrix <T> select_row(int row) throw(out_of_range); // seleziona una riga
-  Matrix <T> select_column (int column) throw(out_of_range); // seleziona una colonna
-  Matrix <T> select_diag(int diag=0)throw (out_of_range);
-  Matrix <T> product (Matrix <T> m1, Matrix <T> m2); // prodotto
-  Matrix <T> sum (Matrix <T> m1, Matrix <T> m2); // somma
-  //<T> size(); // std:: pair per ritornare le dimensioni di righe e colonne
-  int get_col() const ; // ritorna numero colonne
-  int get_rows()const; // ritorna numero colonne
+  int get_col() const ; // n colonne
+  int get_rows()const; // n righe
+  T& getValue(int row,int column) throw (out_of_range);
+  void setValue(int row,int column,const T value)throw (out_of_range);
+  Matrix<T>& trasponse();
+  Matrix<T> Gauss() const throw (invalid_argument);
   int rank() const;
   T min() const;
   T max() const;
-  T det();
-  T getValue(int row, int column) throw (out_of_range); // ritorno un valore in una certa posizione
-  void setValue(int row, int column, T value)throw (out_of_range); // setto un valore in una certa posizione
+  T det() const throw(invalid_argument);
+  Matrix <T> select_row(int row) throw(out_of_range); // seleziona una riga
+  Matrix <T> select_column (int column) throw(out_of_range); // seleziona una colonna
+  Matrix <T> operator *(Matrix <T> &m1) throw (out_of_range); // prodotto.
+  Matrix <T> operator +(const Matrix <T>& m1)throw (out_of_range); // somma
 
 };
+
+
+
 
 template <typename T>
 Matrix <T>::Matrix(int row, int col) throw (out_of_range): rows(row), col(col){
     if (row <= 0 || col <= 0)
-        throw out_of_range ("Index error"); // lancio eccezione poi nel main avrò il blocco try-catch(out_of_range &e){print ("errore")}
+        throw out_of_range ("Index error");
 
     M = new T [row * col];
     for (int i=0; i<row * col;i++)
-        //for (int j=0; j<col; j++)
             M[i]=0;
 }
 
@@ -61,24 +67,48 @@ Matrix<T>::Matrix(int size) throw(out_of_range): rows(size), col(size) {
 
     M = new T [rows * col];
     for (int i=0; i<rows*col;i++)
-        //for (int j = 0; j < col; j++)
             M[i] = 0;
 }
+
+template<typename T>
+Matrix<T>::Matrix(const Matrix<T> &Matrix): rows(Matrix.rows), col(Matrix.col), M(new T[rows*col]) {
+    for (int i=0; i<rows*col; i++)
+        M[i]=Matrix.M[i];
+}
+
 
 template <typename T>
 Matrix<T>::~Matrix() {
     delete[] M;
 }
 
-template <typename T> // tipo ritornato classe :: nome funzione
+template <typename T>
 Matrix<T> Matrix<T>::select_row(int row) throw(out_of_range) {
     if (row<0 || row >= rows)
         throw out_of_range ("row index error");
 
-    Matrix <T> mr = new Matrix(1,col);
+    Matrix <T> mr (1,col);
     for (int i= 0; i < col; i++)
        mr.M[i] = M[row*col+i];
     return mr;
+}
+
+template<typename T>
+Matrix<T> &Matrix<T>::trasponse() {
+    T* traspose = new T[rows*col];
+    for (int i =0; i< rows; i++) {
+        for (int j = 0; j < col; j++)
+            traspose[j * rows + i] = M[i * col + j];
+    }
+    delete[] M;
+    M = traspose;
+    // aggiorno il numero di righe e di colonne dopo l'operazione di trasposizione. Faccio uno swap attraverso una variabile temporanea.
+    int n_rows= rows;
+    rows=col;
+    col=n_rows;
+    return *this;
+
+
 }
 
 template<typename T>
@@ -86,14 +116,14 @@ Matrix<T> Matrix<T>::select_column(int column) throw(out_of_range){
     if (column<0 || column >= col)
         throw out_of_range ("col index error");
 
-    Matrix <T> mc = new Matrix(rows,1);
+    Matrix <T> mc(rows,1);
     for (int i= 0; i < rows; i++)
-        mc.M[i] = M[rows*i];
+        mc.M[i] = M[i*rows+column];
     return mc;
 }
 
 template<typename T>
-int Matrix<T>::get_col() const {
+ int Matrix<T>::get_col() const {
     return col;
 }
 
@@ -121,32 +151,145 @@ T Matrix<T>::max() const {
 }
 
 template<typename T>
-Matrix<T> Matrix<T>::select_diag(int diag) throw (out_of_range){
-   if ((diag<0 && abs(diag) >= rows) || (diag >0 && abs(diag) >= col))
-       throw out_of_range ("diag not found");
-
-   Matrix <T> md = Matrix(rows,col);
-   for (int i=0; i< rows; i++) { // scorro sulle righe per selezionare valori su diag
-       int column = i + diag; //seleziono colonna giusta su cui prendere elemento
-       if (column< col)
-            md.setValue(getValue(i,column),i,column);
-   }
-    return md;
-}
-
-template<typename T>
-T Matrix<T>::getValue(int row, int column)throw (out_of_range){
+T &Matrix<T>::getValue(int row, int column)throw(out_of_range){
     if (row<0 || row >= rows || col<0 || column >= col)
         throw out_of_range ("index error");
-    return M [row * column + col];
+    return M [row * col + column];
 }
 
 template<typename T>
 void Matrix<T>::setValue(int row, int column, T value)throw (out_of_range){
     if (row<0 || row >= rows || col<0 || column >= col)
         throw out_of_range ("index error");
-    M [row * column + col] = value;
+    M [row * col + column] = value;
 }
 
+template<typename T>
+Matrix<T> Matrix<T>::operator+(const Matrix<T> &m1)throw(out_of_range){
+    if (this->rows != m1.rows || this->col != m1.col)
+        throw out_of_range ("size error");
+
+    Matrix <T> sum(rows,col);
+    for (int i=0; i< rows * col; i++)
+        sum.M[i]= this->M[i]+ m1.M[i];
+
+    return sum;
+}
+
+template<typename T>
+Matrix<T> Matrix<T>::operator*(Matrix<T> &m1) throw (out_of_range){ // implemento prodotto riga per colonna
+    if (col != m1.rows)
+        throw out_of_range ("size error for product");
+
+    Matrix <T> prod(rows,m1.col);
+    for(int i=0; i<rows; i++)
+        for (int j=0; j<m1.col; j++)
+            for (int k=0; k< col; ++k)
+                prod.M[i* m1.col+j] += M[i*col+k] * m1.M[k* m1.col+j];
+    return prod;
+}
+
+template<typename T>
+Matrix<T> Matrix<T>::Gauss() const throw (invalid_argument){ // implementazione algoritmo gauss senza pivoting per
+    //ridurre la matrice in una triangolare superiore.
+
+    if (rows !=col)
+        throw invalid_argument("matrix must be square"); // la matrice triangolare è una matrice quadrata.
+
+    Matrix<T> reduced(*this);
+    float m=0;
+
+    for (int k=0; k<rows-1; k++) {
+        if (reduced.M[k * col + k] == 0)
+            throw invalid_argument("value is zero");
+
+        for (int i = k + 1; i < rows; i++) {
+            m = reduced.M[i * col + k] / reduced.M[k * col + k];
+            for (int j = k + 1; j < rows; j++)
+                reduced.M[i * col + j] = reduced.M[i * col + j] - m * reduced.M[k * col + j];
+
+            reduced.M[i * col + k] = 0;
+        }
+    }
+    return reduced;
+}
+
+template<typename T>
+Matrix<T>::Matrix(int rows,int col, const vector<T>& v) throw(out_of_range): rows(rows), col(col){
+    if (rows <= 0 || col <=0)
+        throw out_of_range("invalid rows and cols");
+    M = new T [rows * col];
+
+    if (v.size() != rows*col)
+        throw out_of_range(" Matrix size is different to vector size");
+
+    for (int i =0; i<rows*col; i++)
+        M[i]= v[i];
+}
+
+template<typename T>
+Matrix<T>::Matrix(int size, const vector<T> &v) throw(out_of_range): rows(size), col(size){
+    if (rows <= 0 || col <=0)
+        throw out_of_range("invalid size of rows and cols");
+    M = new T[rows*col];
+
+    if (v.size() != rows * col)
+        throw out_of_range(" Matrix size is different to vector size");
+
+    for (int i = 0; i<rows*col; i++)
+            M[i] = v[i];
+    }
+
+
+template<typename T>
+int Matrix<T>::rank() const {
+
+    int rank = col; // variabile per memorizzare il rango. Inizializzo  al numero totale delle colonne
+    Matrix<T> M_rank(*this);
+    float m=0;
+
+    for (int k=0; k< rank;k++){
+        if (M_rank.M[k*col+k] !=0){
+            for (int i=k+1;i<rows; i++){
+                m = M_rank.M[i*col+k]/ M_rank.M [k*col+k]; // calcolo coefficiente m
+                for (int j=k+1; j<rows; j++)
+                    M_rank.M[i*col+j]=M_rank.M[i*col+j] - m * M_rank.M[k*col+j];
+                M_rank.M[i*col+k]=0; // metto a 0 l'elemento sotto il primo pivot
+            }
+        }
+        else{ // if ()==0. Se l'elemento 00 è 0 allora scambio la prima riga con quella sotto.
+            bool reduced = true; // variabile che mi indica se posso ridurre oppure no
+            for (int i=k+1;i<rows;i++){ //Ciclo per scambiare le righe
+                if (M_rank.M[i*col+k]!=0){
+                    for(int j=0; j<rank;j++){ // scambio tutti i valori della riga con quelli della riga sotto.
+                        T temp= M_rank.M[k*col+j];
+                        M_rank.M[k*col+j]= M_rank.M[i*col+j];
+                        M_rank.M[i*col+j]=temp;
+                    }
+                    reduced = false; // non è stata ridotta. Ho solo scambiato le righe
+                }
+            }
+            if(reduced){
+                rank--;
+                for (int i=0; i<rows; i++)
+                    M_rank.M[i*col+k]=M_rank.M[i*col+rank];
+            }
+            k--;
+        }
+    }
+    return rank;
+}
+
+template<typename T>
+T Matrix<T>::det() const throw(invalid_argument) {
+   if (rows!=col)
+       throw invalid_argument ("Matrix must be square");
+   Matrix <T> m = Gauss();
+   T det = 1; //varabile per mem il det inizializzato a 1.
+    for (int i=0; i<rows; i++){
+        det *= m.getValue(i,i);
+    }
+    return det;
+}
 
 #endif //MATRIXTEMPLATE_MATRIX_H
--- main.cpp
+++ main.cpp
@@ -1,7 +1,116 @@
 #include <iostream>
 #include "Matrix.h"
+using namespace std;
+
+
+
+
 int main() {
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
+
+    Matrix<float> M1(2, {1, 3, 0, 7});
+    Matrix<float> M2(2, {2, 3, 3, 7});
+    Matrix<float> A(3, {1, 3, 0, 7, 8, 0, 1, 3, 4});
+
+
+    cout << "M1 =" << endl;
+    for (int i = 0; i < M1.get_rows(); i++) {
+        cout << "  ";
+        for (int j = 0; j < M1.get_col(); j++) {
+            cout << M1.getValue(i, j) << " ";
+        }
+        cout << endl;
+    }
+
+    Matrix<float> C(M2);
+    cout << "C =" << endl;
+    for (int i = 0; i < C.get_rows(); i++) {
+        cout << "  ";
+        for (int j = 0; j < C.get_col(); j++) {
+            cout << C.getValue(i, j) << " ";
+        }
+        cout << endl;
+    }
+
+    cout << "massimo:" << M1.max() << endl;
+    cout << "minimo:" << M2.min() << endl;
+
+
+    M1.setValue(0, 0, 9);
+    cout << "M1 modificata =" << endl;
+    for (int i = 0; i < M1.get_rows(); i++) {
+        cout << "  ";
+        for (int j = 0; j < M1.get_col(); j++) {
+            cout << M1.getValue(i, j) << " ";
+        }
+        cout << endl;
+    }
+
+    cout << "numero colonne M2: " << M2.get_col() << endl;
+    cout << "numero righe M1: " << M1.get_rows() << endl;
+
+    Matrix<float> M3 = C.Gauss();
+    cout << "M3= " << endl;
+    for (int i = 0; i < M3.get_rows(); i++) {
+        cout << "  ";
+        for (int j = 0; j < M3.get_col(); j++) {
+            cout << M3.getValue(i, j) << " ";
+        }
+        cout << endl;
+    }
+
+    Matrix<float> M4 = M1.select_column(0);
+    cout << "M4= " << endl;
+    for (int i = 0; i < M4.get_rows(); i++) {
+        cout << "  ";
+        for (int j = 0; j < M4.get_col(); j++) {
+            cout << M4.getValue(i, j) << " ";
+        }
+        cout << endl;
+    }
+
+    Matrix<float> M5 = M1.select_row(0);
+    cout << "M5= " << endl;
+    for (int i = 0; i < M5.get_rows(); i++) {
+        cout << "  ";
+        for (int j = 0; j < M5.get_col(); j++) {
+            cout << M5.getValue(i, j) << " ";
+        }
+        cout << endl;
+    }
+
+    Matrix<float> M6= M1+M2;
+    cout << "M6 = " << endl;
+    for (int i = 0; i < M6.get_rows(); i++) {
+        cout << "  ";
+        for (int j = 0; j < M6.get_col(); j++) {
+            cout << M6.getValue(i, j) << " ";
+        }
+        cout << endl;
+    }
+
+    Matrix<float> M7= M1*M2;
+    cout << "M7 = " << endl;
+    for (int i = 0; i < M7.get_rows(); i++) {
+        cout << "  ";
+        for (int j = 0; j < M7.get_col(); j++) {
+            cout << M7.getValue(i, j) << " ";
+        }
+        cout << endl;
+    }
+
+    cout << "det M1 = " <<M1.det()<< endl;
+    cout << "rango A = " <<A.rank()<< endl;
+
+    Matrix<float> Trasponse = A.trasponse();
+    cout << "trasponse = " << endl;
+    for (int i = 0; i < Trasponse.get_rows(); i++) {
+        cout << "  ";
+        for (int j = 0; j < Trasponse.get_col(); j++) {
+            cout << Trasponse.getValue(i, j) << " ";
+        }
+        cout << endl;
+    }
 
 }
+
+
